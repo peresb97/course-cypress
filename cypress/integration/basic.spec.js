8698c156c5948aa2080f39ca6ecebd380c874693
peresb97 <brunodarosaperes1997@gmail.com>
2021-08-22 18:13:12 -0300

    Commit Aula 22 até 34

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -15,6 +15,26 @@ describe('Cypress basics', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .should('contain', 'Campo')
+
+        let syncTitle
+
+        cy.title().then(title => {
+            console.log(title)
+
+            cy.get('#formNome').type(title)
+        
+            syncTitle = title
+        
+        })
+
+        cy.get('[data-cy=dataSobrenome]').then($el => {
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
+        })
+
     })
 
     it('Should find and interact whit an element', () => {
--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -59,7 +59,7 @@ describe('Work with basic elements', () => {
         cy.get("[name='formSexo']").should('have.length', 2)
     })
 
-    it.only('Checkbox', () => {
+    it('Checkbox', () => {
         cy.get('#formComidaPizza')
             .click()
             .should('be.checked')
@@ -73,10 +73,31 @@ describe('Work with basic elements', () => {
         cy.get('[data-test=dataEscolaridade]')
             .select('2o grau completo')
             .should('have.value', '2graucomp')
+
+    
+        cy.get('[data-test=dataEscolaridade] option')
+            .should('have.length', 8)
+        
+        cy.get('[data-test=dataEscolaridade] option').then($arr => {
+            const values = []
+            $arr.each(function() {
+                values.push(this.innerHTML)
+            })
+            expect(values).to.include.members(["Superior", "Mestrado"])
+        })
+
     })
 
-    it.only('Combo multiplo', () => {
+    it('Combo multiplo', () => {
         cy.get('[data-testid=dataEsportes]')
             .select(['natacao', 'Corrida', 'nada'])
+        //cy.get('[data-testid=dataEsportes]').should('have.value', ['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]').then($el => {
+            expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+            expect($el.val()).to.have.length(3)
+        })
+        cy.get('[data-testid=dataEsportes]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
     })
 })
