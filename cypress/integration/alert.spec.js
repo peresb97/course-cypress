5856eb0de690697424e3aff2834fe102607e9e21
peresb97 <brunodarosaperes1997@gmail.com>
2021-08-24 19:12:25 -0300

    Commit Aula 37

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -47,7 +47,7 @@ describe('Work with basic elements', () => {
         cy.get('#confirm').click()
     })
 
-    it.only('Prompt', () => {
+    it('Prompt', () => {
         cy.window().then(win => {
             cy.stub(win, 'prompt').returns('42')
         })
@@ -58,5 +58,23 @@ describe('Work with basic elements', () => {
             expect(msg).to.be.equal(':D')
         })
         cy.get('#prompt').click()
+    })
+
+    it.only('validando mensagens',() => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+        
+        cy.get('#formNome').type('Wagner')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+        cy.get('[data-cy=dataSobrenome]').type('Aquino')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
     })
 })
